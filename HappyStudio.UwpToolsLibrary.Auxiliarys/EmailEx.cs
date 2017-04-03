9beba8867a4f5834a941a345fbc80d1252afc4a3
李继龙 <kljzndx@outlook.com>
2017-04-04 07:26:08 +0800

    自己封装的邮件类

---
+++ HappyStudio.UwpToolsLibrary.Auxiliarys/EmailEx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System;
+
+namespace HappyStudio.UwpToolsLibrary.Auxiliarys
+{
+    public static class EmailEx
+    {
+        public static async Task Send(string to, string subject, string body)
+        {
+            await Launcher.LaunchUriAsync(new Uri($"mailto:{to}?subject={subject}&body={body.Replace("\n", "%0A")}"));
+        }
+    }
+}
