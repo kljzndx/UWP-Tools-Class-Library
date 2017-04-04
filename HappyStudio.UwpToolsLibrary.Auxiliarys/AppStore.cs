3f5254a0448cca25ec2481bd2ffd924ae6657bcf
李继龙 <kljzndx@outlook.com>
2017-04-04 13:37:02 +0800

    自己封装的应用商店类

---
+++ HappyStudio.UwpToolsLibrary.Auxiliarys/AppStore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System;
+
+namespace HappyStudio.UwpToolsLibrary.Auxiliarys
+{
+    public static class AppStore
+    {
+        public static async Task<bool> ShowItAppAsync(string developerName)
+        {
+            return await Launcher.LaunchUriAsync(new Uri($"ms-windows-store://publisher/?name={developerName}"));
+        }
+        public static async Task<bool> WriteReviewAsync(string appStoreID)
+        {
+            return await Launcher.LaunchUriAsync(new Uri($"ms-windows-store://review/?ProductId={appStoreID}"));
+        }
+    }
+}
