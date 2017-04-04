3c3ef1345741ffbbfce23376a5792805aa01eb4f
李继龙 <kljzndx@outlook.com>
2017-04-04 13:37:49 +0800

    自己封装的系统信息类

---
+++ HappyStudio.UwpToolsLibrary.Information/SystemInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.ExchangeActiveSyncProvisioning;
+using Windows.System.Profile;
+
+namespace HappyStudio.UwpToolsLibrary.Information
+{
+    public static class SystemInfo
+    {
+        public static string DeviceType { get => AnalyticsInfo.VersionInfo.DeviceFamily; }
+
+        public static string DeviceName
+        {
+            get
+            {
+                var deviceInfo = new EasClientDeviceInformation();
+                return $"{deviceInfo.SystemManufacturer} {deviceInfo.SystemProductName}";
+            }
+        }
+
+        public static double BuildVersion
+        {
+            get
+            {
+                ulong u = Convert.ToUInt64(AnalyticsInfo.VersionInfo.DeviceFamilyVersion);
+                string version = $"{u >> 16 & 0xFFFF}.{u & 0xFFFF}";
+                return Double.Parse(version);
+            }
+        }
+    }
+}
