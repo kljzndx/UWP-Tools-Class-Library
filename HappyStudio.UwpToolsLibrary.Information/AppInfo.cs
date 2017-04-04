b02db4a8bbd3ce29bf52aedf06f6747b9fec86bd
李继龙 <kljzndx@outlook.com>
2017-04-04 13:38:19 +0800

    自己封装的应用信息类

---
+++ HappyStudio.UwpToolsLibrary.Information/AppInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.UI.Xaml;
+
+namespace HappyStudio.UwpToolsLibrary.Information
+{
+    public static class AppInfo
+    {
+        public static string Name { get => Package.Current.DisplayName; }
+        public static string Version
+        {
+            get
+            {
+                var version = Package.Current.Id.Version;
+                return $"{version.Major}.{version.Minor}.{version.Build}";
+            }
+        }
+    }
+}
