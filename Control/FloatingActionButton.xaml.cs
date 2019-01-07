705780399996525707febee5795d45ec8236d5c1
李继龙 <kljzndx@outlook.com>
2019-01-07 17:27:58 +0800

    添加浮动按钮

---
+++ Control/FloatingActionButton.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//https://go.microsoft.com/fwlink/?LinkId=234236 上介绍了“用户控件”项模板
+
+namespace HappyStudio.UwpToolsLibrary.Control
+{
+    public sealed partial class FloatingActionButton : UserControl
+    {
+        public FloatingActionButton()
+        {
+            this.InitializeComponent();
+        }
+
+        public static readonly DependencyProperty GlyphProperty = DependencyProperty.Register(
+            nameof(Glyph), typeof(string), typeof(FloatingActionButton), new PropertyMetadata('\uE115'));
+
+        public string Glyph
+        {
+            get => (string) GetValue(GlyphProperty);
+            set => SetValue(GlyphProperty, value);
+        }
+
+        public event RoutedEventHandler Click;
+        
+        private void Root_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            Click?.Invoke(this, e);
+        }
+    }
+}
